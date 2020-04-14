bd4c37ca61a1ec743c40d502b6630bdd6ff0f27f
dariusmaximus <dariulqa@wp.pl>
2020-04-14 21:41:22 +0200

    adding product to a cart functionality modified

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,20 +6,26 @@ function Product({ phone }) {
     const { dispatch, userProducts } = useContext(StoreContext);
     const isInAcart = userProducts.some(item => item.id === phone.id);
 
+    //if in a cart do not pass the product to the userProducts so use handleOnclick
+
+    const handleOnclick = () => {
+        return !isInAcart ? dispatch({
+            type: 'ADD_ITEM', item: {
+                id: phone.id,
+                model: phone.model,
+                price: phone.price,
+                img: phone.img,
+                inStock: phone.inStock
+            }
+        }) : ''
+    }
+
     return (
         <div className="product">
             <h3 className="product-model">{phone.model}</h3>
             <p className="product-price">{phone.price}</p>
             <img className="product-img" src={phone.img} alt="mobile phone"></img>
-            <button className="regular-btn" onClick={() => dispatch({
-                type: 'ADD_ITEM', item: {
-                    id: phone.id,
-                    model: phone.model,
-                    price: phone.price,
-                    img: phone.img,
-                    inStock: phone.inStock
-                }
-            })}>{isInAcart ? 'Already in a cart' : 'Add to cart'}</button>
+            <button className="regular-btn" onClick={() => handleOnclick()}>{isInAcart ? 'Already in a cart' : 'Add to cart'}</button>
         </div>
     );
 }
